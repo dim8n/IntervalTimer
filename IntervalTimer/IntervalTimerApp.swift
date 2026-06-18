27fa78be670e3fefb8a0b9f5221e592764fa75e5
dim8n <d.elizarov@gmail.com>
2026-06-19 00:25:36 +0300

    Initial Commit

---
+++ IntervalTimer/IntervalTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntervalTimerApp.swift
+//  IntervalTimer
+//
+//  Created by Елизаров Дмитрий on 19.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct IntervalTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
